07e111613670b73c668336f1ff044e449f4272c6
Kevin Quach <kevin.quach@gmail.com>
2013-08-25 19:11:32 -0400

    add chances and guess word

--- hangmanKQ.rb
+++ hangmanKQ.rb
@@ -1,17 +1,20 @@
-WORDS = %w(orange tomato corn)
+WORDS = %w(orange tomato)
 
-@player_container = []
-@word_container = []
-@current_guess = nil
-# Ask for a guess from user
-# Choose random word
-@random_word = WORDS.sample
+
+  @player_container = []
+  @word_container = []
+  @current_guess = nil
+  @chances = 8
+  # Ask for a guess from user
+  # Choose random word
+  @random_word = WORDS.sample
+
 
 # Get length of random word and create array of underscores
 
-hidden = Array.new(@random_word.length, '_').join
+@hidden = Array.new(@random_word.length, '_').join
 
-puts "Word: #{hidden}"
+puts "Word: #{@hidden}"
 puts "\nRandom word: #{@random_word}\n"
 
 # random_word.split(//).each do |letter|
@@ -19,32 +22,53 @@ puts "\nRandom word: #{@random_word}\n"
 # end
 
 # print @word_container
-
-def guess
-  puts "\nGuess a signle letter (a-z) or the entire word: "
-  input = gets.chomp
-  @current_guess = input
-  @player_container << input
-  check_letter(@current_guess)
+def check_for_a_word(word)
+  if @random_word == word
+    puts "Nice, #{word} is correct! You win!"
+    exit
+  else
+    check_letter(word)
+  end
 end
 
-def check_letter(letter)
-  if @random_word.include?(letter)
-
-    puts "You guessed #{letter} and it's one of the letters!"
-    puts "Found #{@random_word.count(letter)} occurances(s) of the character #{letter}"
-    index(letter)
-    guess
+def check_chances
+  if @chances == 0
+    puts "No chances left. Game Over!"
+    exit
   else
-    puts "You guessed wrong!"
     guess
   end
 end
 
-
 def index(letter)
-  puts @random_word.index(letter)
+  index_num = @random_word.index(letter)
+  @hidden[index_num] = letter
+  print @hidden
 end
+
+def check_letter(letter)
+  if @random_word.include?(letter)
+    puts "You guessed #{letter} and it's one of the letters!"
+    puts "Found #{@random_word.count(letter)} occurances(s) of the character '#{letter}'"
+    index(letter)
+    guess
+  else
+    puts "You guessed wrong!"
+    @chances -= 1
+    check_chances
+  end
+end
+
+def guess
+  puts "\nGuess a single letter (a-z) or the entire word: "
+  puts "Chances remaining: #{@chances}"
+  input = gets.chomp
+  @current_guess = input
+  @player_container << input
+  check_for_a_word(input)
+end
+
+
 
 
 guess
