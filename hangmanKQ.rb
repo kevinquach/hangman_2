ebfdfbedb5114e0d1211e1e854ec66cb7f9a0c35
Kevin Quach <kevin.quach@gmail.com>
2013-08-26 09:09:32 -0400

    clean up code

--- hangmanKQ.rb
+++ hangmanKQ.rb
@@ -1,79 +1,88 @@
-WORDS = %w(orange tomato)
+WORDS = %w(orange)
 
-
-  @player_container = []
-  @word_container = []
-  @current_guess = nil
+  @word = WORDS.sample
+  @hidden_letters = Array.new(@word.length, '_').join
+  @letters_already_guessed = []
+  @counter = 0
   @chances = 8
-  # Ask for a guess from user
-  # Choose random word
-  @random_word = WORDS.sample
+  @current_guess = nil
 
+puts "\nRandom word: #{@word}\n"
 
-# Get length of random word and create array of underscores
-
-@hidden = Array.new(@random_word.length, '_').join
-
-puts "Word: #{@hidden}"
-puts "\nRandom word: #{@random_word}\n"
-
-# random_word.split(//).each do |letter|
-#   @word_container << letter
-# end
-
-# print @word_container
-def check_for_a_word(word)
-  if @random_word == word
-    puts "Nice, #{word} is correct! You win!"
+def check_for_the_word
+  if @word == @current_guess
+    puts "Nice, '#{@current_guess}' is correct! You win!"
     exit
   else
-    check_letter(word)
+    wrong_guess
   end
 end
 
 def check_chances
   if @chances == 0
-    puts "No chances left. Game Over!"
+    puts "\nNo chances left. Game over...Better luck next time!"
     exit
   else
-    guess
+    prompt
   end
 end
 
-def index(letter)
-  index_num = @random_word.index(letter)
-  @hidden[index_num] = letter
-  print @hidden
-end
-
-def check_letter(letter)
-  if @random_word.include?(letter)
-    puts "You guessed #{letter} and it's one of the letters!"
-    puts "Found #{@random_word.count(letter)} occurances(s) of the character '#{letter}'"
-    index(letter)
-    guess
+def check_solved
+  if @hidden_letters == @word
+    puts "The word is indeed '#{@word}'.\n\nYou got all the letters! Good job!"
+    exit
   else
-    puts "You guessed wrong!"
-    @chances -= 1
-    check_chances
+    prompt
   end
+end
+
+def wrong_guess
+  puts "Sorry, that isn't correct. Keep trying!"
+  @chances -= 1
+  check_chances
+end
+
+def reveal_letter
+
+  index_num = @word.index(@current_guess)
+  @hidden_letters[index_num] = @current_guess
+  check_solved
+end
+
+def correct_guess
+
+  @letters_already_guessed << @current_guess
+  puts "You guessed '#{@current_guess}' and we found #{@word.count(@current_guess)} occurences(s) of the letter\n\n"
+  reveal_letter
+end
+
+def letter_already_guessed?
+  if @letters_already_guessed.include?(@current_guess)
+    puts "You already guessed '#{@current_guess}'. Pick another!"
+    prompt
+  else
+    correct_guess
+  end
+end
+
+def check_letter
+  @word.include?(@current_guess) ? letter_already_guessed? : wrong_guess
 end
 
 def guess
-  puts "\nGuess a single letter (a-z) or the entire word: "
-  puts "Chances remaining: #{@chances}"
-  input = gets.chomp
-  @current_guess = input
-  @player_container << input
-  check_for_a_word(input)
+  @current_guess = gets.chomp.downcase.strip
+  if (/\S/).match(@current_guess) && @current_guess.size == 1
+    check_letter
+  elsif @current_guess.size > 1
+    check_for_the_word
+  else
+    prompt
+  end
 end
 
+def prompt
+  puts "Word: #{@hidden_letters}\nChances remaining: #{@chances}\nGuess a single letter (a-z) or the entire word: "
+  guess
+end
 
-
-
-guess
-
-
-
-
-
+prompt
